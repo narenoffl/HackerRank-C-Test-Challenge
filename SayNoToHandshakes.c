2fb1fbfc6927a70641d93a5830eda9e4c77c58c1
Naren Purushothaman <naren.p.2018.cse@ritchennai.edu.in>
2020-07-04 11:47:28 +0530

    Test5C
    Before the outbreak of corona virus to the world, a meeting happened in a room in Wuhan. A person who attended that meeting had COVID-19 and no one in the room knew about it! So everyone started shaking hands with everyone else in the room as a gesture of respect and after meeting unfortunately every one got infected! Given the fact that any two persons shake hand exactly once, Can you tell the total count of handshakes happened in that meeting?
    
    Say no to shakehands. Regularly wash your hands. Stay Safe.
    
    Input Format 
    The first line contains the number of test cases T, T lines follow. 
    Each line then contains an integer N, the total number of people attended that meeting.
    
    Output Format
    
    Print the number of handshakes for each test-case in a new line.
    
    Constraints 
    
    1 <= T <= 1000 
    0 < N < 106

---
+++ SayNoToHandshakes.c
@@ -0,0 +1,16 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n,res;
+    scanf("%d",&n);
+    res = n*(n-1)/2;
+    printf("%d",res);
+
+}
