141f46900e4499ff404659ea780f6afd91a1a8a3
Naren Purushothaman <naren.p.2018.cse@ritchennai.edu.in>
2021-06-18 21:10:31 +0530

    ArrayIntro - Jenny Lectures

---
+++ ArrayIntro.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+    int a[5],i;
+    printf("\nEnter the elements\n");
+    for(i=0;i<4;i++)
+    scanf("%d",a);
+    
+    
+
+    return 0;
+}
